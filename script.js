f45ed000ba4a158f4b861172cd40f24cf2be2c3c
Sumit Saha <sumitsaha1233@gmail.com>
2023-02-09 13:04:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+menu.addEventListener("click",()=>{
+    console.log("Button cliked")
+    nav.style.display = "flex";
+    menu.style.display = "none"
+
+})
+
+closebtn.addEventListener("click",()=>{
+    console.log("Button cliked")
+    nav.style.display = "none";
+    menu.style.display = "block"
+
+})
+
